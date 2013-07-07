45136b6879f02ec15d43d3d0ca8b521e34f07523
Pitsanu Swangpheaw <pitsanu_s@hotmail.com>
2013-07-07 11:11:18 +0700

    use tweetService in post

--- src/main/java/com/minitwitter/controller/UserController.java
+++ src/main/java/com/minitwitter/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.minitwitter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.minitwitter.service.TweetService;
 
 @Controller
 public class UserController {
+	@Autowired
+	TweetService tweetService;
 	@RequestMapping("/")
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("index");
@@ -21,16 +28,21 @@ public class UserController {
 		return mv;
 	}
 	
-	@RequestMapping("/{username}/post")
-	public ModelAndView post(@PathVariable String username) {		
+	@RequestMapping(value = "/{username}/post", method = RequestMethod.POST)
+	public ModelAndView post(@PathVariable String username, @RequestParam String message) {		
 		ModelAndView mv = new ModelAndView("post");
-		//tweetService.tweet(username, message);
-		System.out.println("post as: " + username);
+		if (message != null && message != "") {
+			tweetService.tweet(username, message);
+			System.out.println("post as: " + username);
+			System.out.println("with message: " + message);
+		} else {
+			System.out.println("post empty message as: " + username);			
+		}		
 		return mv;
 	}
 	
-	@RequestMapping("/{username}/delete/{postId}")
-	public ModelAndView post(@PathVariable String username, @PathVariable String postId) {		
+	@RequestMapping(value = "/{username}/delete/{postId}", method = RequestMethod.POST)
+	public ModelAndView delete(@PathVariable String username, @PathVariable String postId) {		
 		ModelAndView mv = new ModelAndView("delete");
 		// tweetService.delete(postId);
 		System.out.println("post as: " + username);
